adcb39f470937fc8b6ab7e2edbb134d0f8edc9e0
Yoshiaki Hara <>
2021-08-16 20:40:20 +0900

    change font family and color.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
